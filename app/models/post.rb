459825de526de5caa6aa11c6f1880c9ce19d665b
Lisa Lee <snickers495@gmail.com>
2017-12-05 16:38:06 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,14 +3,8 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :post_tags
   has_many :tags, :through => :post_tags
-  accepts_nested_attributes_for :tags, reject_if: proc { |tag_attributes| tag_attributes['name'].blank? }
+  accepts_nested_attributes_for :tags, reject_if: :all_blank
 
   validates_presence_of :name, :content
 
-  def tags_attributes=(tags_attributes)
-    tags_attributes.each do |tags_attributes|
-      tag = Tag.find_or_create_by(tags_attributes)
-      self.tags << tag
-    end
-  end
 end
